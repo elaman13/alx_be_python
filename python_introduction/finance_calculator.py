127732cf0654ed89e43365ff094c25cf733dcbea
Elyas <elaman99619@gmail.com>
2025-07-08 21:26:04 +0300

    personal finance fixed bugs

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,8 +3,13 @@ montly_expenses = int(input('Enter your total monthly expenses: '))
 
 monthly_savings = monthly_income - montly_expenses
 
-projected_savings = monthly_savings * 12 + (monthly_savings * 0.05 * 12)
+annual_interest_rate = 0.05
 
+savings_without_interest = monthly_savings * 12
+
+interest_money = savings_without_interest * annual_interest_rate
+
+projected_savings = savings_without_interest + interest_money
 
 print('Your monthly savings are $', monthly_savings)
 print('Projected savings after one year, with interest, is: $', projected_savings)
