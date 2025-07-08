181418dd39b80b8d145834adb8b3f4ed5162c75c
Elyas <elaman99619@gmail.com>
2025-07-08 19:46:18 +0300

    fixed error

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,5 +1,13 @@
-number1, number2 = 10, 5
+number1 = 10
+number2 = 5
 
-print('Addition of ', number1, ' and ', number2, ' is ', number1 + number2)
-print('Subtraction of ', number1, ' and ', number2, ' is ', number1 - number2)
-print('Multiplication of ', number1, ' and ', number2, ' is ', number1 * number2)
+sum = number1 + number2
+
+difference = number1 - number2
+
+product = number1 * number2
+
+
+print('Addition of ', number1, ' and ', number2, ' is ', sum)
+print('Subtraction of ', number1, ' and ', number2, ' is ', difference)
+print('Multiplication of ', number1, ' and ', number2, ' is ', product)
