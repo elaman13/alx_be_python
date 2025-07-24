55b86b0a6cbc43486869416e6999d8ca164e48b4
Elyas <elaman99619@gmail.com>
2025-07-24 10:15:22 +0300

    fns and dsa added

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,14 @@
-def perform_operation(num1: float, num2: float, operation: str):
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            return num1 / num2 if num2 != 0 else 'Cannot divide by zero'
-        case _:
-            return 'Invalid operation'
+def perform_operation(num1, num2, operation):
+  
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "cannot divide by zero"
+        return num1 / num2
+    else:
+        return "Invalid Input"
