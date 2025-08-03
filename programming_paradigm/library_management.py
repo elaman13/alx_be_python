cb78718ed23432aa4a3c69b80ef4cb6cfd41cd3b
Elyas <elaman99619@gmail.com>
2025-08-03 20:59:42 +0300

    updated

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -22,13 +22,13 @@ class Book:
 
 class Library:
     def __init__(self):
-        self.__books = []
+        self._books = []
     
     def add_book(self):
-        self.__books.append(book)
+        self._books.append(book)
 
     def check_out_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title:
                 if book.check_out():
                     print(f"'{title}' has been checked out.")
@@ -38,7 +38,7 @@ class Library:
         print(f"Book titled '{title}' not found in the library.")
 
     def return_book(self, title):
-        for book in self.__books:
+        for book in self._books:
             if book.title == title:
                 if book.return_book():
                     print(f"'{title}' has been returned.")
@@ -48,7 +48,7 @@ class Library:
         print(f"Book titled '{title}' not found in the library.")
 
     def list_available_books(self):
-        available_books = [book for book in self.__books if book.is_available()]
+        available_books = [book for book in self._books if book.is_available()]
         if not available_books:
             print("No books are currently available.")
         else:
