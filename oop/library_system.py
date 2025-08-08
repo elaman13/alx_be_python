88deb4ec221cbc837500e6e029afd8f94bd4c7a6
Elyas <elaman99619@gmail.com>
2025-08-08 11:54:54 +0300

    class inheritance with magic methods

---
+++ oop/library_system.py
@@ -0,0 +1,33 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
+class EBook(Book):
+    def __init__(self, title, author, file_size: int):
+        self.file_size = file_size
+        super().__init__(title=title, author=author)
+    
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count: int):
+        self.page_count = page_count
+        super().__init__(title=title, author=author)
+
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+class Library:
+    books = []
+    
+    def add_book(self, book):
+        Library.books.append(book)
+    
+    def list_books(self):
+        for book in Library.books:
+            print(book)
