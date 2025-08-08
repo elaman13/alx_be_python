6dfb8e4df3201e099220bac8bd3c2558f2f79d2b
Elyas <elaman99619@gmail.com>
2025-08-08 12:00:46 +0300

    update

--- oop/library_system.py
+++ oop/library_system.py
@@ -23,7 +23,8 @@ class PrintBook(Book):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
-    self.books = []
+    def __init__(self):
+        self.books = []
     
     def add_book(self, book):
         Library.books.append(book)
